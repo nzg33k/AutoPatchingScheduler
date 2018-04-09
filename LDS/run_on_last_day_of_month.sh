75c7b6c757ef0cac8280d298d1851864a159db7c
Stephen Ward <swar051@ldsappprd01.its.auckland.ac.nz>
2018-04-09 13:25:14 +1200

    Added script to call script from cron.

---
+++ LDS/run_on_last_day_of_month.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#You should call this from cron, something like 
+#1  0 28-31 * * /path/to/run_on_last_day_of_month.sh
+
+[ "$(date +%d -d 'tomorrow')" == "01" ] && python `dirname $0`/api.py
+

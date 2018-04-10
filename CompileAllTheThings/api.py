411785ac5a8fb784a5be536f30f1d5237969b7d7
Stephen Ward <swar051@itssshprd01.its.auckland.ac.nz>
2018-04-11 09:26:07 +1200

    Adding the Compile All The Things project.  I hope to get basic data from multiple sources and comile it.
    I feel dirty.

---
+++ CompileAllTheThings/api.py
@@ -0,0 +1,14 @@
+'''
+This is my attempt to gather some basic information from multiple places and compile it.
+
+Wish me luck.
+'''
+
+import xmlrpclib
+import datetime
+import random
+import string
+from landscape_api.base import API as landscape_api
+#This file should be based on configuration.py.template
+import configuration as conf
+

c7f9ae3492a0e40e417f38abf22f49f0dd84be55
nzg33k <sw_github@nzgeek.com>
2018-04-11 12:24:08 +1200

    Added code to get the system names from LDS and RHS5

--- CompileAllTheThings/api.py
+++ CompileAllTheThings/api.py
@@ -5,10 +5,33 @@ Wish me luck.
 '''
 
 import xmlrpclib
-import datetime
-import random
-import string
 from landscape_api.base import API as landscape_api
 #This file should be based on configuration.py.template
 import configuration as conf
 
+def get_lds_computer_names():
+    "Get all the computers in LDS"
+    computers = landscape_api(conf.uri, conf.key, conf.secret, conf.ca).get_computers() # pylint: disable=no-member
+    computernames = []
+    for computer in computers:
+        computernames.append(computer["hostname"].encode("utf-8"))
+    return computernames
+
+def get_rhs5_computer_names():
+    "Get all the computers in RHS5"
+    client = xmlrpclib.Server(conf.satellite_url, verbose=0)
+    key = client.auth.login(conf.satellite_login, conf.satellite_password)
+    computers = client.system.listSystems(key)
+    computernames = []
+    for computer in computers:
+        computernames.append(computer["name"].encode("utf-8"))
+    return computernames
+
+def get_computer_names():
+    "Get the names of the computers"
+    names = get_lds_computer_names()
+    names += get_rhs5_computer_names()
+    return names
+
+#print len(get_computer_names())
+print get_computer_names()

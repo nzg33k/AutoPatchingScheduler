cd8f95c6c234515197ffaf3189839b0723d7bb14
nzg33k <sw_github@nzgeek.com>
2018-04-12 14:28:55 +1200

    Fixed case of constants, added a disable proxy option

--- CompileAllTheThings/api.py
+++ CompileAllTheThings/api.py
@@ -12,7 +12,7 @@ import configuration as conf
 
 def get_lds_computer_names():
     "Get all the computers in LDS"
-    computers = landscape_api(conf.uri, conf.key, conf.secret, conf.ca).get_computers() # pylint: disable=no-member
+    computers = landscape_api(conf.URI, conf.KEY, conf.SECRET, conf.CA).get_computers() # pylint: disable=no-member
     computernames = []
     for computer in computers:
         computernames.append(computer["hostname"].encode("utf-8"))
@@ -20,8 +20,8 @@ def get_lds_computer_names():
 
 def get_rhs5_computer_names():
     "Get all the computers in RHS5"
-    client = xmlrpclib.Server(conf.satellite_url, verbose=0)
-    key = client.auth.login(conf.satellite_login, conf.satellite_password)
+    client = xmlrpclib.Server(conf.SATELLITE_URL, verbose=0)
+    key = client.auth.login(conf.SATELLITE_LOGIN, conf.SATELLITE_PASSWORD)
     computers = client.system.listSystems(key)
     computernames = []
     for computer in computers:
@@ -30,8 +30,11 @@ def get_rhs5_computer_names():
 
 def get_web_computer_names():
     "Get a list of names from a weblist"
+    #Weird proxy stuff here, this will work around it by bypassing the proxy
+    if conf.DISABLEPROXY:
+        urllib2.getproxies = lambda: {}
     computernames = []
-    for weburl in conf.weblisturl:
+    for weburl in conf.WEBLISTURL:
         computers = urllib2.urlopen(weburl)
         for computer in computers:
             computernames.append(computer.encode("utf-8").rstrip())

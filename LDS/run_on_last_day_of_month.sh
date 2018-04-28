75984bc6a1dc40c1ccd99b39b13dcda29cdcc8f1
Stephen Ward <swar051@IT331558.local>
2018-04-28 13:13:42 +1200

    Changed the name of the script so the cron task must also change

--- LDS/run_on_last_day_of_month.sh
+++ LDS/run_on_last_day_of_month.sh
@@ -3,5 +3,5 @@
 #You should call this from cron, something like 
 #1  0 28-31 * * /path/to/run_on_last_day_of_month.sh
 
-[ "$(date +%d -d 'tomorrow')" == "01" ] && python `dirname $0`/api.py
+[ "$(date +%d -d 'tomorrow')" == "01" ] && python `dirname $0`/schedule_LDS_patches.py
 

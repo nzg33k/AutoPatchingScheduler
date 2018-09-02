331f8b1ac7445cd14385654456d83f634e0825b0
Stephen Ward <s.ward@auckland.ac.nz>
2018-09-03 06:46:12 +1200

    Scripts called from the pythin script were using the PWD rather than the scripts directory so I've changed this to use pushd.

--- RHS5/run_on_last_day_of_month.sh
+++ RHS5/run_on_last_day_of_month.sh
@@ -3,4 +3,4 @@
 #You should call this from cron, something like 
 #1  0 28-31 * * /path/to/run_on_last_day_of_month.sh
 
-[ "$(date +%d -d 'tomorrow')" == "01" ] && python `dirname $0`/schedule_RHS5_patches.py
+[ "$(date +%d -d 'tomorrow')" == "01" ] && pushd `dirname $0` > /dev/null && python schedule_RHS5_patches.py && popd > /dev/null
